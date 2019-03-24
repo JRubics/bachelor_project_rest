29a738df3bad603f7b647b42b014845f64a96c48
Jelena Dokic <jelena.dpk@gmail.com>
2019-03-24 21:50:05 +0100

    token link

--- bachelor_project_rest/urls.py
+++ bachelor_project_rest/urls.py
@@ -20,5 +20,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('token/', obtain_auth_token, name='api_token_auth'),
 ]
