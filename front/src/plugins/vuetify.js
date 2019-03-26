f784c372886f87c1cd3b176b78c252f602575e8c
Jelena Dokic <jelena.dpk@gmail.com>
2019-03-26 22:19:35 +0100

    colors

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -6,9 +6,9 @@ import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.use(Vuetify, {
 	theme: {
-		primary: '#392648',
+		primary: '#704f6e',
 		secondary: '#282828',
-		accent: '#458588',
+		accent: '#547660',
 		error: '#cc241d',
 		info: '#458588',
 		success: '#98971a',
