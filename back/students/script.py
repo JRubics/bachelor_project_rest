d6a217684d0180d5c1128d05f468072d80ee8944
Jelena Dokic <jelena.dpk@gmail.com>
2019-03-31 23:24:22 +0200

    docker in docker

--- back/students/script.py
+++ back/students/script.py
@@ -3,3 +3,4 @@ import docker
 def run_container():
     client = docker.from_env()
     # print(client.containers.run("bfirsh/reticulate-splines", detach=True))
+    # print(client.containers.list())
