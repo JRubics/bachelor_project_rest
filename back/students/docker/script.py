6ce9f93e7521ea54723f01012522e60f975073bb
Jelena Dokic <jelena.dpk@gmail.com>
2019-04-04 20:59:35 +0200

    remove image when finished

--- back/students/docker/script.py
+++ back/students/docker/script.py
@@ -3,11 +3,12 @@ import os
 
 def run_container(callback, filepath, tag):
     client = docker.from_env()
-    image, logs = client.images.build(path = "./students/docker", tag=tag, dockerfile='./Dockerfile', buildargs={'filepath': filepath, 'filename': os.path.basename(filepath)})
+    image, logs = client.images.build(path = "./students/docker", tag=tag, dockerfile='./Dockerfile', rm=True, buildargs={'filepath': filepath, 'filename': os.path.basename(filepath)})
     callback(image)
 
 def finished(image):
     print('finished')
     print(image)
-    # client = docker.from_env()
-    # client.containers.run(image, detach=True)
+
+    client = docker.from_env()
+    client.images.remove(image=image.id)
