3753faf3f7e2f60226f857d4160e2e64c508214c
Jelena Dokic <jelena.dpk@gmail.com>
2019-04-24 16:55:15 +0200

    docker

--- back/students/docker/script.py
+++ back/students/docker/script.py
@@ -5,13 +5,13 @@ def build_image(filepath, tag):
     client = docker.from_env()
     repository = os.environ.get('REGISTRY_URL', '') + '/assignments'
 
+    auth_client = client.login(username=os.environ.get('REGISTRY_USERNAME', ''), password=os.environ.get('REGISTRY_PASSWORD', ''), registry=os.environ.get('REGISTRY_URL', ''))
+    print('auth done')
+
     print('build start')
     image, logs = client.images.build(path="./students/docker", dockerfile='./Dockerfile', rm=True, buildargs={'filepath': filepath, 'filename': os.path.basename(filepath)})
     image.tag(repository=repository, tag=tag)
     print('build end')
-
-    auth_client = client.login(username=os.environ.get('REGISTRY_USERNAME', ''), password=os.environ.get('REGISTRY_PASSWORD', ''), registry=os.environ.get('REGISTRY_URL', ''))
-    print('auth done')
 
     client.images.push(repository=repository, tag=tag)
     print('push done')
