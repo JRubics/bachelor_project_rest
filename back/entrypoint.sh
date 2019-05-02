38e3c6855f7730735bacc2a3da57a62cd0400e1c
Jelena Dokic <jelena.dpk@gmail.com>
2019-05-02 22:07:16 +0200

    timeout

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python3 manage.py migrate
-gunicorn --bind 0.0.0.0:80 bachelor_project_rest.wsgi:application --log-level=debug
+gunicorn --bind 0.0.0.0:80 bachelor_project_rest.wsgi:application --log-level=debug --timeout 100
